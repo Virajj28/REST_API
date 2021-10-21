ea08ca2dbe7fd8ccffc923076c109fead8b19fae
Viraj Jadhav <viraj28j@gmail.com>
2021-10-21 18:35:38 +0530

    connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://nervebody:nervebody28@cluster0.w22uk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  () =>
+    console.log('Connected to MongoDB Atlas!')
+  )
 
 // Import Routes
 const authRoute = require('./routes/auth');
